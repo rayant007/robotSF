058cfcc76c0bf34ed6874b3a77bfb4688a0762cb
rayant007 <rayant.oo0s@gmail.com>
2023-04-30 01:38:15 -0600

    agreado comunicacion y se cambia codigo en rotbotSF

--- robotSF.py
+++ robotSF.py
@@ -1,1 +1,12 @@
-print("hola")
+import serial
+import time
+
+ard = serial.Serial('/dev/ttyACM0',9600,timeout=2)
+time.sleep(2)
+print('puerto valido')
+cadena = 'hola'
+cadenaBytes = cadena.encode('utf-8')
+ard.write(cadenaBytes)
+
+
+ard.close
