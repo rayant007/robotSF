494ce840e386a46e35c7e6f061dba7d96c743964
rayant007 <rayant.oo0s@gmail.com>
2023-05-09 12:51:45 -0600

    se agrega guiRobotSF.py

---
+++ guiRobotSF.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+# Crear una instancia de la ventana
+ventana = tk.Tk()
+
+# Personalizar la ventana
+ventana.title("RobotController")
+ventana.geometry("1080x720")  # ancho x alto en píxeles
+
+# Ejecutar el bucle principal de la ventana
+ventana.mainloop()
